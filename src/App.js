60bf720802faf7d49f878a8ddc92dc4d46732511
shmablo <Pramos187@hotmail.com>
2024-09-28 16:13:50 -0400

    Added routing to all page links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
+//Pages for the Router
 import TierList from "./Pages/TierList/TierList"; // Import the newly created TierList component
+import Home from "./Pages/Home";
+import About from "./Pages/About/About";
+import Playbook from "./Pages/Playbook/Playbook";
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/tier-list" element={<TierList />} />
-        {/* Add other routes here */}
+        <Route path="/about" element={<About />} />
+        <Route path="/playbook" element={<Playbook />} />
       </Routes>
     </Router>
   );
---
+++ src/Pages/Playbook/Playbook.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Playbook() {
+    return (
+        <div>
+            <h1>Playbook</h1>
+            <p>map</p>
+        </div>
+    );
+};
+
+export default Playbook;
