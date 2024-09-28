dc0144c4c0df3020342c1fd6a2318e9624361feb
shmablo <Pramos187@hotmail.com>
2024-09-27 21:20:07 -0400

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import Header from './Components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <p>Welcome to Deadplant</p>
-      <Info />
     </div>
   );
 }
 
-function Info() {
-  return (
-    <div>
-      <h1>Inventory System</h1>
-      <p>Manage your Stuff.</p>
-    </div>
-  );
-}
+// function Info() {
+//   return (
+//     <div>
+//       <h1>Inventory System</h1>
+//       <p>Manage your Stuff.</p>
+//     </div>
+//   );
+// }
 export default App;
