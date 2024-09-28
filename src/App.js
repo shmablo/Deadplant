7def7dba51f857738eea40f2efe9a4a342c38dda
furzaan <furzaanjkhan@gmail.com>
2024-09-28 13:45:48 -0400

    Added router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
-import "./App.css";
+import TierList from "./Pages/TierList/TierList"; // Import the newly created TierList component
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <p>Welcome to Deadplant</p>
-    </div>
+      <Routes>
+        <Route path="/tier-list" element={<TierList />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
   );
 }
 
-// function Info() {
-//   return (
-//     <div>
-//       <h1>Inventory System</h1>
-//       <p>Manage your Stuff.</p>
-//     </div>
-//   );
-// }
 export default App;
