5c0b3cc5991de35552e401e9f9799c4644099da7
Alex Zorrilla <alexzorrilla13@gmail.com>
2024-09-29 05:59:23 -0400

    Revert "Merge branch 'main' of https://github.com/shmablo/Deadplant"
    This reverts commit 01964fb7fdfc98b58a40ad00d0e309c46c23c9e5, reversing
    changes made to 28fb18f3f078b3fa255ff266592b8608dedcb7a1.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Amplify } from 'aws-amplify'; // Ensure correct named import of Amplify
-import awsConfig from './aws-exports'; // Import your AWS configuration
-
 import Header from "./Components/Header/Header";
+
+// Pages for the Router
 import TierList from "./Pages/TierList/TierList";
 import Home from "./Pages/Home";
 import About from "./Pages/About/About";
 import Playbook from "./Pages/Playbook/Playbook";
 import Logout from "./Pages/Logout/Logout";
-import Login from "./Pages/Login/Login"; // Ensure correct import of Login component
-import ProtectedRoute from './Components/ProtectedRoute'; // Ensure correct import of ProtectedRoute component
+// Footer
 
-// Configure Amplify
-Amplify.configure(awsConfig);
 
 function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tier-list" element={<TierList />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/playbook" element={
-          <ProtectedRoute>
-            <Playbook />
-          </ProtectedRoute>
-        } />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <main className="main-content"> {/* Add the main-content class here */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tier-list" element={<TierList />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/playbook" element={<Playbook />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
+      </main>
+ 
     </Router>
   );
 }
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,26 +1,10 @@
-import { Amplify } from 'aws-amplify';
-const awsConfig = {
+const awsconfig = {
     Auth: {
-        // REQUIRED - Amazon Cognito Identity Pool ID
-        identityPoolId: 'us-east-2:758cfaf9-cb55-49d9-9a88-e9e965f3480a', // e.g. us-east-2:12345678-xxxx-xxxx-xxxx-xxxxxxxxxxxx
-        // REQUIRED - Amazon Cognito Region
         region: 'us-east-2',
-        // OPTIONAL - Amazon Cognito User Pool ID
-        userPoolId: 'us-east-2_W4jFGPN1K', // e.g. us-east-2_xxxxxx
-        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-        userPoolWebClientId: '41dkf3t774e4qr7k45lvmtdvnq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    }
+        userPoolId: 'us-east-2_W4jFGPN1K',
+        userPoolWebClientId: '41dkf3t774e4qr7k45lvmtdvnq',
+        identityPoolId: 'Yus-east-2:758cfaf9-cb55-49d9-9a88-e9e965f3480a',
+    },
 };
 
-export default awsConfig;
-
-// const awsconfig = {
-//     Auth: {
-//         region: 'us-east-2',
-//         userPoolId: 'us-east-2_W4jFGPN1K',
-//         userPoolWebClientId: '41dkf3t774e4qr7k45lvmtdvnq',
-//         identityPoolId: 'us-east-2:758cfaf9-cb55-49d9-9a88-e9e965f3480a',
-//     },
-// };
-
-// export default awsconfig;
+export default awsconfig;
