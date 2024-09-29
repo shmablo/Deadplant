ed31dc3d6b5f58f98274ecc67a859b113d1577b5
shmablo <Pramos187@hotmail.com>
2024-09-28 23:33:52 -0400

    Created the Home page and fixed header bugs

---
+++ src/aws-exports.js
@@ -0,0 +1,10 @@
+const awsconfig = {
+    Auth: {
+        region: 'us-east-2',
+        userPoolId: 'us-east-2_W4jFGPN1K',
+        userPoolWebClientId: '41dkf3t774e4qr7k45lvmtdvnq',
+        identityPoolId: 'Yus-east-2:758cfaf9-cb55-49d9-9a88-e9e965f3480a',
+    },
+};
+
+export default awsconfig;
