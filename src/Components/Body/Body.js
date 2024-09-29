386810bc6bca8271aa2762293fb9d420fe042343
shmablo <Pramos187@hotmail.com>
2024-09-28 23:14:20 -0400

    Created a Home Page and Fixed Header Bugs

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,23 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css'; //Import for pencil icon
 import './Body.css';
 
 const Body = () => {
   return (
     <div className="body-container">
-      <h1>BODY</h1>
-      <div className="image-container">
-        <div className="haze-container">
-          <img alt="agent1" src="haze.png" />
+      <video autoPlay loop muted className="background-video">
+        <source src="deadlockGameplay.mp4" type="video/mp4" />
+      </video>
+      <div className="text-overlay">
+        <div className="text-container">
+          <h1>Deadplant</h1>
+          <p>The only <span className="highlight">Deadlock strategy tool</span> designed from
+            <br /> the ground up for the competitive scene. Make teams,
+            <br /> outsmart your opponents, and become a playmaker.
+          </p>
+          <Link to="/playbook">
+            <button className="styled-button">
+              <i className="fas fa-pencil-alt"></i> Start Creating
+            </button>
+          </Link>
         </div>
-        <div className="paradox-container">
-          <img alt="agent2" src="paradox.png" />
-          </div>
-        </div>
-        <div className="bottomText">
-            <p>bottom text</p>
-          </div>
       </div>
-    
+      {/* <div className="image-container">
+      </div>
+      <div className="bottomText">
+      </div> */}
+    </div>
+
   );
 };
 
